cfaab948cefc84a6ad02f1345f0ee2462511c90e
La7rodectus <54902922+La7rodectus@users.noreply.github.com>
2021-08-10 15:38:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT; //3000;
 
 app.use(express.static('./'));
 app.use(express.json({ limit: '1mb' }));
